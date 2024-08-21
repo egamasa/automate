db156469bdbbb06f68adb2d6779bcf19e9d963d6
egamasa <40469431+egamasa@users.noreply.github.com>
2024-08-21 14:38:33 +0900

    add gcp-pubsub-line-notify

---
+++ gcp-pubsub-line-notify/README.md
@@ -0,0 +1,55 @@
+# Pub/Sub notify to LINE bot
+
+Scripts for Google Cloud Functions (FaaS of Google Cloud Platform).  
+Send Pub/Sub payload-text to talkroom of LINE bot.
+
+## Configurations
+
+### Create LINE channel
+
+- Regist to [LINE Developers Console](https://developers.line.biz/console/) as a developer.
+- Create a new provider.
+- Create a new channel.
+- Issue a long-term access token of the channel.
+
+### env.yaml
+
+- `PROJECT_ID`
+  - GCP project ID that place this function.
+- `SECRET_NAME`
+  - Secret name of GCP Secret Manager that saved secrets of LINE channel.
+
+### secrets.json
+
+- `channel_access_token`
+  - Channel access token of LINE channel.
+
+## Deploy
+
+### Add secret to GCP Secret Manager
+
+```
+$ gcloud secrets create line-bot-secrets \
+    --data-file=secrets.json \
+    --locations=asia-northeast1 \
+    --replication-policy=user-managed
+```
+
+### Create Pub/Sub topic for trigger
+
+```
+$ gcloud pubsub topics create line-notify-topic
+```
+
+### Deploy to Google Cloud Functions
+
+```
+$ gcloud functions deploy pubsub-line-notify \
+    --region=asia-northeast1 \
+    --runtime=ruby32 \
+    --memory=128MB \
+    --timeout=60 \
+    --entry-point=function \
+    --trigger-topic=line-notify-topic \
+    --env-vars-file=env.yaml
+```
---
+++ gcp-pubsub-line-notify/app.rb
@@ -0,0 +1,61 @@
+# coding: utf-8
+
+require "base64"
+require "functions_framework"
+require "google/cloud/secret_manager"
+require "json"
+require "net/http"
+require "uri"
+
+class Line
+  URI = URI.parse('https://api.line.me/v2/bot/message/broadcast')
+
+  def get_secrets()
+    project_id = ENV['PROJECT_ID']
+    secret_name = ENV['SECRET_NAME']
+
+    client = Google::Cloud::SecretManager.secret_manager_service
+    key = client.secret_version_path(
+      project: project_id,
+      secret: secret_name,
+      secret_version: 'latest'
+    )
+    res = client.access_secret_version(
+      name: key
+    )
+    return JSON.parse(res.payload.data)
+  end
+
+  def make_broadcast_request(data)
+    secrets = get_secrets()
+    token = secrets['channel_access_token']
+    req = Net::HTTP::Post.new(URI.path)
+    req["Content-Type"] = "application/json"
+    req["Authorization"] = "Bearer #{token}"
+    req.body = data.to_json
+    return req
+  end
+
+  def send_broadcast(msg)
+    data = {
+      "messages" => [
+        {
+          "type" => "text",
+          "text" => msg
+        }
+      ]
+    }
+    http = Net::HTTP.new(URI.host, URI.port)
+    req = make_broadcast_request(data)
+    res = Net::HTTP.start(URI.hostname, URI.port, use_ssl: URI.scheme == "https") do |https|
+      https.request(req)
+    end
+  end
+end
+
+# Cloud Functions entry point -> function
+FunctionsFramework.cloud_event 'function' do |event|
+  msg = Base64.decode64(event.data["message"]["data"])
+  line = Line.new
+  line.send_broadcast(msg.force_encoding("UTF-8"))
+end
